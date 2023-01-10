a06e879c87d1aad6a6d69901688e2e5eecdd8e6b
Misha Panivnyk <panivnykm@gmail.com>
2023-01-10 21:41:51 +0200

    add create Selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,13 @@
-export const getFilterList = state => {
-  const normalizedFilter = state.filters.toLowerCase();
-  return state.contacts.items.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
-};
+import { createSelector } from '@reduxjs/toolkit';
+import { getContacts } from './contacts/selectors';
+import { getFilter } from './filter/selectors';
+
+export const getFilterList = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
