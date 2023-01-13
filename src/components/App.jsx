d6b7947f74569b5d9c473d082dd53e9392571cf8
Misha Panivnyk <panivnykm@gmail.com>
2023-01-13 20:04:05 +0200

    refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import ContactForm from 'components/ContactForm/ContactForm';
-import { getContacts } from 'redux/contacts/selectors';
-import { useSelector } from 'react-redux';
+import {
+  getContacts,
+  selectError,
+  selectIsLoading,
+} from 'redux/contacts/selectors';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container, Title, SubTitle, Message } from 'components/App.styled';
-
+import { fetchContacts } from 'redux/operations';
+import { useEffect } from 'react';
 export default function App() {
   const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <Container>
       <Title>Phonebook</Title>
       <ContactForm />
       <SubTitle>Contacts</SubTitle>
       {contacts.length > 0 && <Filter />}
-      {contacts.length === 0 && <Message>There is not any contacts</Message>}
+      {!isLoading && !error && contacts.length === 0 && (
+        <Message>There is not any contacts</Message>
+      )}
       <ContactList />
     </Container>
   );
